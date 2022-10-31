ff30e9dea69abd8b35e3b5e682044c75fbc43dec
asmfahim <asmfahim1@gmail.com>
2022-10-31 16:27:19 +0600

    local database learning

---
+++ lib/reusable_textfield.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class CsCommonTextFieldWidget extends StatefulWidget {
+  const CsCommonTextFieldWidget(
+      {this.titleText = '',
+      this.titleTextAlign = TextAlign.center,
+      required this.isPassword,
+      required this.hintText,
+      required this.textController});
+
+  final String titleText;
+  final TextAlign titleTextAlign;
+  final bool isPassword;
+  final String hintText;
+  final TextEditingController textController;
+
+  @override
+  _CsCommonTextFieldWidgetState createState() =>
+      _CsCommonTextFieldWidgetState();
+}
+
+class _CsCommonTextFieldWidgetState extends State<CsCommonTextFieldWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      obscureText: widget.isPassword,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.all(10.0),
+        hintText: widget.hintText, // pass the hint text parameter here
+        hintStyle: TextStyle(color: Colors.black26),
+      ),
+      style: TextStyle(color: Colors.black),
+    );
+  }
+}
